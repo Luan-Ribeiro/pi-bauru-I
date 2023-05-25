db9156070c1f077855fc49ed95184b13804cbf62
Luan <luan_ribeiro@outlook.com>
2023-05-25 17:17:55 -0300

    Fix deploy error

--- core/settings.py
+++ core/settings.py
@@ -128,6 +128,8 @@ USE_TZ = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = ('static',)
